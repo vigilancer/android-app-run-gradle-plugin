81b97917b47bff0da9722945674a9ab8dd56e8ba
Andrey Elizarov <vigilancer@gmail.com>
2017-01-02 15:44:02 +0400

    small cleanup

--- src/main/kotlin/ae/vigilancer/gradle/plugin/android/run/AppRunnerPlugin.kt
+++ src/main/kotlin/ae/vigilancer/gradle/plugin/android/run/AppRunnerPlugin.kt
@@ -9,37 +9,45 @@ import org.gradle.api.tasks.StopExecutionException
 
 class AppRunnerPlugin : Plugin<Project> {
 
+    val cmd_monkey_mobile = "shell monkey -p %s -c android.intent.category.LAUNCHER 1"
+    val cmd_monkey_tv = "shell monkey -p %s -c android.intent.category.LEANBACK_LAUNCHER 1"
+
     override fun apply(project: Project) {
         if (!project.plugins.hasPlugin(AppPlugin::class.java)) {
             throw StopExecutionException("should be applied after 'com.android.application' plugin")
         }
         project.extensions.create("appRunner", AppRunnerExtension::class.java)
         val ext: AppExtension = project.extensions.getByType(AppExtension::class.java)
+        val adb = ext.adbExe
 
-        ext.applicationVariants.all { v ->
-            val taskName = "run${v.name.capitalize()}"
-            val parentTask = v.install
-            val adb = ext.adbExe
-
+        ext.applicationVariants.all { variant ->
             // skipping unsigned non-debug builds since they don't have 'Install*' tasks
-            if (v.isSigningReady) {
-                val packageId = v.applicationId
+            if (!variant.isSigningReady)
+                return@all
 
-                val t: Exec = project.task(
-                    mapOf(
-                        "type" to Exec::class.java,
-                        "dependsOn" to parentTask,
-                        "description" to "Install and run ${v.description}.",
-                        "group" to "Running"
-                    ),
-                    taskName) as Exec
-                if ((project.extensions.getByName("appRunner") as AppRunnerExtension).tv) {
-                    t.setCommandLine(adb, "shell", "monkey", "-p", packageId, "-c", "android.intent.category.LEANBACK_LAUNCHER", "1")
-                } else {
-                    t.setCommandLine(adb, "shell", "monkey", "-p", packageId, "-c", "android.intent.category.LAUNCHER", "1")
-                }
-            }
+            val taskName = "run${variant.name.capitalize()}"
+            val packageId = variant.applicationId
+            val parentTask = variant.install
+
+            val isTvApp = (project.extensions.getByName("appRunner") as AppRunnerExtension).tv
+
+            val cmd: Exec = project.task(
+            mapOf(
+                "type" to Exec::class.java,
+                "dependsOn" to parentTask,
+                "description" to "Install and run ${variant.description}.",
+                "group" to "Running"
+            ),
+            taskName) as Exec
+            cmd.setExecutable(adb)
+            cmd.setArgs(adbArguments(packageId, isTvApp))
         }
+    }
+
+    private fun adbArguments(packageId: String, isTvApp: Boolean): Iterable<Any> {
+        val cmd = if (isTvApp) cmd_monkey_tv else cmd_monkey_mobile
+
+        return cmd.format(packageId).split(" ")
     }
 }
 
